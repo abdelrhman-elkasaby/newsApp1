ebe34f619d48b79bf3cda6e80d41fb159e32564b
abdelrhman-elkasaby <abdelrhman.elkasaby96@gmail.com>
2021-04-28 01:52:27 +0200

    small changes

--- lib/screens/newsDetails/newsDetails.dart
+++ lib/screens/newsDetails/newsDetails.dart
@@ -44,17 +44,20 @@ class _NewsDetailsState extends State<NewsDetails> with NewsDetailsData{
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Image.network(state.data.articles[widget.index].urlToImage,width: MediaQuery.of(context).size.width,height: MediaQuery.of(context).size.height*0.25,),
+                      const SizedBox(height: 15,),
                       Container(
                           width: MediaQuery.of(context).size.width,
                           child: Text(state.data.articles[widget.index].description,style: TextStyle(fontSize: 18),textDirection: TextDirection.rtl,textAlign: TextAlign.start,)),
+                      const SizedBox(height: 15,),
                       Container(
                         width: MediaQuery.of(context).size.width,
                         child: Text('يمكنك قراءة الخبر كامل عن طريق هذا اللينك : ',textDirection: TextDirection.rtl,),
                       ),
+                      const SizedBox(height: 5,),
                       Container(
                         width: MediaQuery.of(context).size.width,
                         child: InkWell(
-                          child: Text('${state.data.articles[widget.index].url}',textDirection: TextDirection.rtl,),
+                          child: Text('${state.data.articles[widget.index].url}',textDirection: TextDirection.rtl,style: TextStyle(color: Colors.blue),),
                           onTap: (){
                             Navigator.of(context).push(MaterialPageRoute(builder: (context)=>WebViewScreen(state.data.articles[widget.index].url,state.data.articles[widget.index].source.name)));
                           },
